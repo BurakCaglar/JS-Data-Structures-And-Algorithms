966dae82e95cd2fd042d012b364687ea07899316
BurakCaglar <burakcaglar@yandex.com>
2021-04-27 00:33:35 +0300

    completed

--- exercises/palindrome/index.js
+++ exercises/palindrome/index.js
@@ -7,6 +7,10 @@
 //   palindrome("abba") === true
 //   palindrome("abcdefg") === false
 
-function palindrome(str) {}
+function palindrome(str) {
+  return str.split("").every((char, index) => {
+    return char === str[str.length - index - 1];
+  });
+}
 
 module.exports = palindrome;
